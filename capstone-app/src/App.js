703bf2adf46531d45ccd04941a122d606d1c16f2
James Nathaniel F Tan <120952619+Jemssu@users.noreply.github.com>
2025-09-17 13:46:42 +0800

    Sample Push

--- capstone-app/src/App.js
+++ capstone-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/admin" element={<AdminPage />} /> {/* optional */}
+        <Route path="/admin" element={<AdminPage />} /> {/* PUSHED */}
       </Routes>
     </Router>
   );
